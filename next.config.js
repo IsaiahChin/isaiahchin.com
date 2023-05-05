c5e12ce90a9d040ae2d3d7d9d7708441ef37590f
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-05 19:11:47 +1200

    Updated config files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-}
+  poweredByHeader: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
