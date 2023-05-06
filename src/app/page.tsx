ff233e523aac04d4b5abfa0dd474ea0850d15b9c
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-07 00:54:45 +1200

    Added blurb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 export default function Home() {
   return (
-    <section className="py-32">
-      <p className="py-4">
-        Hey, I'm <strong>Isaiah</strong>.
-      </p>
-      <p>A developer with an eye for detail.</p>
-    </section>
+    <>
+      <section className="pt-0 py-16 w-fit">
+        <h1 className="py-4 text-5xl">
+          Hey, I'm <span className="font-semibold">Isaiah</span>
+        </h1>
+        <p>A design-driven developer with an eye for details.</p>
+        <br />
+        <p>
+          I aspire to build responsive and aesthetically pleasing software that
+          helps others. I like planning, solving problems, playing the piano,
+          thinking... and gaming.
+        </p>
+      </section>
+    </>
   );
 }
