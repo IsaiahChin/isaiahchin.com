0dacf0930f823ff31e08c2bdd47a0ec25233bd48
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-07 00:55:16 +1200

    Added assets

---
+++ src/assets/ExternalLinkIcon.js
@@ -0,0 +1,32 @@
+export default function ExternalLinkIcon() {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" className="scale-75">
+      <title>https://github.com/isaiahchin</title>
+      <desc>My GitHub</desc>
+      <defs />
+      <g
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+        strokeLinecap="round"
+      >
+        <g
+          transform="translate(-312.000000, -203.000000)"
+          stroke="#333333"
+          strokeWidth="2"
+          className="stroke-slate-900 dark:stroke-stone-100"
+        >
+          <g transform="translate(312.000000, 203.000000)">
+            <path d="M20,15 L20,18.0026083 C20,19.1057373 19.1073772,20 18.0049107,20 L5.99508929,20 C4.8932319,20 4,19.1073772 4,18.0049107 L4,5.99508929 C4,4.8932319 4.89585781,4 5.9973917,4 L9,4" />
+            <polyline
+              strokeLinejoin="round"
+              points="13 4 20.0207973 4 20.0207973 11.0191059"
+            />
+            <path d="M19,5 L12,12" strokeLinejoin="round" />
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
---
+++ src/assets/UpArrowIcon.js
@@ -0,0 +1,31 @@
+export default function UpArrowIcon() {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" className="scale-75">
+      <g
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+        strokeLinecap="round"
+      >
+        <g
+          transform="translate(-488.000000, -1046.000000)"
+          stroke="#333333"
+          strokeWidth="2"
+          className="stroke-slate-900 dark:stroke-stone-100"
+        >
+          <g transform="translate(48.000000, 1046.000000)">
+            <g transform="translate(440.000000, 0.000000)">
+              <path d="M12,4 L12,21" id="Path-58" />
+              <polyline
+                strokeLinejoin="round"
+                transform="translate(12.000000, 7.000000) scale(-1, 1) translate(-12.000000, -7.000000) "
+                points="4 11 12 3 20 11"
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
