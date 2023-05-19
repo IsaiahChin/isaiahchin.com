b53b51e405a9bfc05f8d1d2dd5ecb912f7e9aa96
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-19 22:11:07 +1200

    Updated Section component params + styling

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,3 +1,20 @@
-export default function Section({ children }: { children: React.ReactNode }) {
-  return <section className="mt-8 pt-0 py-16 w-fit">{children}</section>;
+export default function Section({
+  children,
+  id,
+  className,
+}: {
+  children: React.ReactNode;
+  id?: String;
+  className?: String;
+}) {
+  return (
+    <section
+      id={`${id ? id : ''}`}
+      className={`${
+        className ? className : ''
+      } inline-block my-8 sm:my-16 w-full`}
+    >
+      {children}
+    </section>
+  );
 }
