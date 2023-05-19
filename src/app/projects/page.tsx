4e255519898ab0d4f579bf288b1e799a6f707f0e
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-19 22:11:24 +1200

    Added temp project page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,17 @@
+import Section from '@/components/Section';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Projects',
+};
+
+export default function Projects() {
+  return (
+    <Section>
+      <h1>Projects</h1>
+      <p>
+        <i>Coming soon...</i>
+      </p>
+    </Section>
+  );
+}
