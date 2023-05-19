7c66790508d7a2c018afa2e2dbcce79855de4865
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-19 22:07:44 +1200

    Added project section to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import ProjectCard from '@/components/ProjectCard';
 import Section from '@/components/Section';
+import { projects } from '../data/projects.js';
 
 export default function Home() {
   return (
     <>
-      <Section>
-        <h1 className="pb-4 leading-tight">Hi, I'm Isaiah</h1>
+      <Section id="home" className="scroll-mt-[100vh]">
+        <h1>Hi, I'm Isaiah</h1>
         <p>A keen-eyed developer with a passion for UI/UX design.</p>
         <br />
         <p className="max-w-[70ch]">
@@ -12,6 +14,19 @@ export default function Home() {
           I like planning, solving problems, photography, playing the piano, and
           gaming.
         </p>
+      </Section>
+      <Section id="projects">
+        <h1 className="before:content-['/'] before:text-accent pb-12">
+          Projects
+        </h1>
+        <div
+          id="project-container"
+          className="grid sm:grid-cols-[repeat(2,_1fr)] gap-4"
+        >
+          {projects.map((project, index) => (
+            <ProjectCard key={index} project={project} />
+          ))}
+        </div>
       </Section>
     </>
   );
