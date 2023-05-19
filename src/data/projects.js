3157c90f043b1724ad8ad690d9637ec657de6d2d
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-19 22:05:36 +1200

    Added project data

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+  {
+    title: 'isaiahchin.com',
+    description: 'This website!',
+    dateStarted: '2023',
+    repo: 'https://github.com/IsaiahChin',
+    link: '#',
+  },
+  {
+    title: 'Grounded Bestiary',
+    description: 'A creature lookup/analysis tool for the game Grounded',
+    dateStarted: 'WIP',
+    repo: '#',
+    link: '',
+  },
+  {
+    title: 'D&D Spellbook',
+    description: 'A web-based Dungeons and Dragons spell tool',
+    dateStarted: 'Coming soon',
+    repo: '',
+    link: '#',
+  },
+];
