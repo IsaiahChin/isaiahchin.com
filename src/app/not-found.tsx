74f41cca5b8cae476b9e7ac0b7a759e29a4053fb
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2023-05-20 15:28:35 +1200

    Updated 404 link style #2

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
       <p>This is a page I haven't put any content on.</p>
       <Link
         href="/"
-        className="dark:text-accent underline underline-offset-4 hover:decoration-accent transition-all"
+        className="dark:text-accent underline underline-offset-4"
       >
         Here's one I have.
       </Link>
