8a6c5ec3189ffe80268360bc8d29ed19cec4e71d
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2025-03-01 21:59:31 +1300

    Combined name into header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,6 @@ export default function Home() {
   return (
     <>
       <Section id="home" className="scroll-mt-[100vh]">
-        <h2 className="hidden sm:block mb-4">
-          Isaiah Chin<span className="dark:text-accent">.</span>
-        </h2>
         <p>A keen-eyed developer, passionate about UI/UX design.</p>
         <p className="max-w-[65ch] mt-7">
           I aspire to create responsive and intuitive software that helps
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,11 +4,11 @@ import ThemeToggle from "./ThemeToggle";
 
 export default function Header() {
   return (
-    <header className="flex justify-between sm:justify-end items-center w-full py-6 bg-inherit sticky top-0 z-50">
-      <h2 className="block sm:hidden">
-          Isaiah Chin<span className="dark:text-accent">.</span>
-        </h2>
-      <nav className="w-fit flex justify-end items-center gap-2 px-4 py-2 rounded-2xl">
+    <header className="flex justify-between items-center w-full py-6 sm:mt-12 bg-inherit sticky top-0 z-50">
+      <h2>
+        Isaiah Chin<span className="dark:text-accent">.</span>
+      </h2>
+      <nav className="w-fit flex justify-end items-center gap-2 py-2 rounded-2xl">
         <GitHubButton />
         {/* <EmailButton /> */}
         <ThemeToggle />
