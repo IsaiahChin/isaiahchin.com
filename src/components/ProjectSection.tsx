ce833dca36c8422cabe03ca69ab90ecde2d2600b
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2025-03-01 21:42:07 +1300

    Added forward-slash to Projects heading

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -5,7 +5,9 @@ export default function ProjectSection() {
   return (
     <Section id="projects">
       <div className="flex justify-between items-start">
-        <h2 className="mb-10">Projects</h2>
+        <h2 className="before:content-['/'] before:text-accent mb-10">
+          Projects
+        </h2>
       </div>
       <div className="flex flex-col gap-12">{generateProjectCards()}</div>
     </Section>
