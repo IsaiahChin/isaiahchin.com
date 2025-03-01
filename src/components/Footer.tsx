71967df5bc991b68cd755faf09f8221cd1c7b23d
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2025-03-01 22:46:24 +1300

    🎣

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,18 @@
 // import { FaArrowUp } from "react-icons/fa6"
 import Image from "next/image";
-import Penguin from "../assets/penguin-512x512.png";
+import Penguin from "@/assets/penguin-512x512.png";
+import FishingRod from "@/assets/fishing-rod.svg";
 
 export default function Footer() {
   return (
     <footer className="flex w-full py-8 justify-between">
       <p>
-        Built by{" "}
         <span className="underline underline-offset-4 decoration-dashed dark:decoration-accent group cursor-help">
-          Isaiah
+          <Image src={FishingRod} alt="Fishing Rod" />
           <Image
             src={Penguin}
             alt="Penguin"
-            className="fixed -bottom-40 left-1/4 max-w-[8rem] w-fit sm:ml-[11.8rem] sm:pointer-events-none group-hover:-translate-y-24 transition-transform"
+            className="fixed -bottom-40 left-1/4 max-w-[8rem] w-fit -scale-x-100 2xl:scale-100 sm:pointer-events-none group-hover:-translate-y-24 transition-transform"
           />
         </span>
       </p>
