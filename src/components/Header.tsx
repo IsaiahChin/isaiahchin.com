d2ed460266ecec4a99ead082892df545f5e7e2d1
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2025-03-01 21:36:12 +1300

    Replaced pages with button links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,16 @@
-"use client";
-
-import { RxExternalLink } from "react-icons/rx";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
+import EmailButton from "./EmailButton";
+import GitHubButton from "./GitHubButton";
 import ThemeToggle from "./ThemeToggle";
 
 export default function Header() {
-  const pathname = usePathname();
-
-  const navLinks = [
-    { title: "Home", link: "/", isExternal: false },
-    // { title: 'Blog', link: '/blog', isExternal: false },
-    { title: "Projects", link: "/projects", isExternal: false },
-    {
-      title: "GitHub",
-      link: "https://github.com/IsaiahChin",
-      isExternal: true,
-    },
-  ];
-
   return (
-    <header className="w-full bg-inherit py-4 sm:py-8 text-[16px] sm:text-xl sticky top-0 z-50">
-      <nav className="flex justify-between items-center">
-        <ul className="flex gap-4 items-center">
-          {navLinks.map(({ title, link, isExternal }) => (
-            <li key={title} className="pr-4">
-              <Link
-                href={link}
-                className="underline-offset-4 hover:decoration-accent relative inline-flex gap-1 group"
-                target={`${isExternal ? "_blank" : "_self"}`}
-              >
-                {title}
-                {pathname === link ? (
-                  <span className="absolute -bottom-1 w-full h-[2px] bg-accent"></span>
-                ) : (
-                  <span className="absolute -bottom-1 w-full h-0 bg-accent transition-[height] group-hover:h-[2px]"></span>
-                )}
-                {isExternal && <RxExternalLink size={"1.3rem"} />}
-              </Link>
-            </li>
-          ))}
-        </ul>
+    <header className="flex justify-between sm:justify-end items-center w-full py-6 bg-inherit sticky top-0 z-50">
+      <h2 className="block sm:hidden">
+          Isaiah Chin<span className="dark:text-accent">.</span>
+        </h2>
+      <nav className="w-fit flex justify-end items-center gap-2 px-4 py-2 rounded-2xl">
+        <GitHubButton />
+        <EmailButton />
         <ThemeToggle />
       </nav>
     </header>
