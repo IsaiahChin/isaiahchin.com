abfdc8c3ab3afbb8a8149304a8b1ecb443341cb7
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2025-03-02 07:00:03 +1300

    Made header name a link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import GitHubButton from "./GitHubButton";
 import ThemeToggle from "./ThemeToggle";
 
@@ -5,7 +6,9 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center w-full py-6 sm:mt-12 bg-inherit sticky top-0 z-50">
       <h2>
-        Isaiah Chin<span className="dark:text-accent">.</span>
+        <Link href="/">
+          Isaiah Chin<span className="dark:text-accent">.</span>
+        </Link>
       </h2>
       <nav className="w-fit flex justify-end items-center gap-2 py-2 rounded-2xl">
         <GitHubButton />
