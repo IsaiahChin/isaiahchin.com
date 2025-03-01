6115f6bd90853f8bad13dc141f2700126c060f70
Isaiah Chin <68924638+IsaiahChin@users.noreply.github.com>
2025-03-01 21:52:00 +1300

    Hid email button

--- src/components/EmailButton.tsx
+++ src/components/EmailButton.tsx
@@ -4,12 +4,11 @@ import { MdOutlineEmail } from "react-icons/md";
 
 export default function EmailButton() {
   return (
-    <button
-      type="button"
-      onClick={() => navigator.clipboard.writeText("hello@isaiahchin.com")}
+    <a
+      href="mailto:hello@isaiahchin.com"
       className="bg-inherit p-2 rounded-xl hover:scale-125 active:scale-100 transition-transform"
     >
       <MdOutlineEmail size="1.6rem" />
-    </button>
+    </a>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         </h2>
       <nav className="w-fit flex justify-end items-center gap-2 px-4 py-2 rounded-2xl">
         <GitHubButton />
-        <EmailButton />
+        {/* <EmailButton /> */}
         <ThemeToggle />
       </nav>
     </header>
